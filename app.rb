bfc78e96cd6d1dd36868bc690f33dadc40ab3906
Rachel Grier <rgrier104@gmail.com>
2019-08-15 00:29:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,10 @@ class App < Sinatra::Base
     erb :index
   end
 
+  post '/puppy' do
+    @new_puppy = Puppies.new(params[:name],params[:breed],params[:age])
+    
+    erb :display_puppy
+  end
+
 end
